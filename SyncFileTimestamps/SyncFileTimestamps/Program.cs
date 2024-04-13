e2c6c24c50f2840108bdba5f65071e5603c5da80
alxhu-dev <32041622+alxhu-dev@users.noreply.github.com>
2024-04-13 09:40:59 +0200

    Finished basic application

--- SyncFileTimestamps/SyncFileTimestamps/Program.cs
+++ SyncFileTimestamps/SyncFileTimestamps/Program.cs
@@ -4,7 +4,27 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            if (args.Length == 2)
+            {
+                Sync(args[0], args[1]);
+            }
+        }
+
+
+        static void Sync(string sourcePath, string destinationPath)
+        {
+            FolderComparer folderComparer = new FolderComparer();
+            foreach(FilePair filePair in folderComparer.GetFilePairs(sourcePath, destinationPath))
+            {
+                FileInfo sourceFileInfo = new FileInfo(filePair.SourcePath);
+                FileInfo destinationFileInfo = new FileInfo(filePair.DestinationPath);
+
+                Console.Write($"Setting {filePair.DestinationPath.Substring(destinationPath.Length)} ... ");
+                destinationFileInfo.LastWriteTime = sourceFileInfo.LastWriteTime;
+                destinationFileInfo.CreationTime = sourceFileInfo.CreationTime;
+                destinationFileInfo.LastAccessTime = sourceFileInfo.LastAccessTime;
+                Console.WriteLine("Done!");
+            }
         }
     }
 }
