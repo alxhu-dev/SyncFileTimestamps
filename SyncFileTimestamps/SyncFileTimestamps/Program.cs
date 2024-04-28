f34e3c60755fe4088f2a66313a4eaae87d905d99
alxhu-dev <32041622+alxhu-dev@users.noreply.github.com>
2024-04-28 07:34:37 +0200

    Added parallelism

--- SyncFileTimestamps/SyncFileTimestamps/Program.cs
+++ SyncFileTimestamps/SyncFileTimestamps/Program.cs
@@ -2,6 +2,8 @@
 {
     internal class Program
     {
+        private const int MAX_PARALLEL = 4;
+
         static void Main(string[] args)
         {
             if (args.Length == 2)
@@ -13,18 +15,38 @@
 
         static void Sync(string sourcePath, string destinationPath)
         {
+            List<Task> taskList = new List<Task>();
+
             FolderComparer folderComparer = new FolderComparer();
             foreach(FilePair filePair in folderComparer.GetFilePairs(sourcePath, destinationPath))
             {
-                FileInfo sourceFileInfo = new FileInfo(filePair.SourcePath);
-                FileInfo destinationFileInfo = new FileInfo(filePair.DestinationPath);
+                if (taskList.Count >= MAX_PARALLEL)
+                    RemoveFinishedFromList();
 
-                Console.Write($"Setting {filePair.DestinationPath.Substring(destinationPath.Length)} ... ");
-                destinationFileInfo.LastWriteTime = sourceFileInfo.LastWriteTime;
-                destinationFileInfo.CreationTime = sourceFileInfo.CreationTime;
-                destinationFileInfo.LastAccessTime = sourceFileInfo.LastAccessTime;
-                Console.WriteLine("Done!");
+                taskList.Add(Task.Run(() => HandleFilePair(filePair)));
             }
+
+            while (taskList.Any())
+                RemoveFinishedFromList();
+
+            void RemoveFinishedFromList()
+            {
+                Task<Task> waitTask = Task.WhenAny(taskList);
+                Task completedTask = waitTask.Result;
+
+                taskList.Remove(completedTask);
+            }
+        }
+
+        static void HandleFilePair(FilePair filePair)
+        {
+            FileInfo sourceFileInfo = new FileInfo(filePair.SourcePath);
+            FileInfo destinationFileInfo = new FileInfo(filePair.DestinationPath);
+
+            destinationFileInfo.LastWriteTime = sourceFileInfo.LastWriteTime;
+            destinationFileInfo.CreationTime = sourceFileInfo.CreationTime;
+            destinationFileInfo.LastAccessTime = sourceFileInfo.LastAccessTime;
+            Console.WriteLine($"Set {Path.GetFileName(filePair.DestinationPath)}!");
         }
     }
 }
